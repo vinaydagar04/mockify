2c78d2f7c32c488494734d6e8cbb4f9a0e20352c
Vinay Kumar <vinaydagar514@gmail.com>
2026-03-30 13:08:01 +0530

    Initialize the project - mockify

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Button variant="gold" size="hero">
+        Vinay
+      </Button>
+    </div>
+  );
+}
